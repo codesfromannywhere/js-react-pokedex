81a97bdede0becf3104f65d2703338473bd6a86e
LanaxCode <lana.supercode@gmail.com>
2023-04-25 14:02:58 +0200

    new toggle

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,24 +1,32 @@
 // Components
 import PokeList from "../components/PokeList";
-import LightDarkMode from "../components/LightDarkMode";
-import { useState } from "react";
+import Menu from '../components/Menu';
+// import LightDarkMode from "../components/LightDarkMode";
+// import { useState } from "react";
+
+
+
+
 
 const Home = () => {
 
-  const [theme, setTheme] = useState('light');
+  // const [theme, setTheme] = useState('light');
 
-  const toggleTheme = () => {
-    if (theme === 'light') {
-      setTheme('background');
-    } else {
-      setTheme('light');
-    }
-  };
+  // const toggleTheme = () => {
+  //   if (theme === 'light') {
+  //     setTheme('background');
+  //   } else {
+  //     setTheme('light');
+  //   }
+  // };
 
   return (
-    <div className={`${theme}`}>
-      <LightDarkMode toggle={toggleTheme} />
-      <PokeList />
+    <div className="home">
+      {/* <LightDarkMode toggle={toggleTheme} /> */}
+      <PokeList>
+
+        <Menu />
+      </PokeList>
     </div>
   )
 }
